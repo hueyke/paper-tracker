dc0221e5fbad3bed1a1dd4d8bfe634da7356ae9b
Chun-Yu Ke <ck659@cornell.edu>
2018-07-26 00:35:10 -0400

    gmail.py now supports older python

--- gmail.py
+++ gmail.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import smtplib
-from email.message import EmailMessage
+from email.mime.text import MIMEText
 
 def send(status):
     gmail_user = 'hueyke@gmail.com'
     gmail_password = 'rmqwaiecdtppcosm'
     
-    msg = EmailMessage()
-    msg.set_content('Hi Huey,\n\nScience status changed to \"%s\".\n\nBest,\nHuey' % status)
+    msg = MIMEText('Hi Huey,\n\nScience status changed to \"%s\".\n\nBest,\nHuey' % status)
     msg['Subject'] = 'Science status changed to \"%s\"!' % status
     msg['From'] = gmail_user
-    msg['To'] = [gmail_user]
+    msg['To'] = gmail_user
     
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
         server.login(gmail_user, gmail_password)
-        server.send_message(msg)
+        server.sendmail(gmail_user, [gmail_user], msg.as_string())
         server.close()
     
         print('Email sent!')
